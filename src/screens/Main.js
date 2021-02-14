64399950d0b3020e94126e72f8f35f2eeb2306e5
LSaber36 <L.Saber36@gmail.com>
2021-02-14 02:37:29 -0500

    Updated main page buttons and styles

--- src/screens/Main.js
+++ src/screens/Main.js
@@ -3,17 +3,22 @@ import { View, Text } from 'react-native';
 import { Button } from 'react-native-elements';
 
 const Main = ({ navigation }) => {
-
-	const { textStyle, container } = styles;
+	const { textStyle, container, buttonContainer, buttons } = styles;
 
 	return (
 		<View style = { container }>
-			<Text>Main Page</Text>
-			<Button
-				title = 'Log Out'
-				buttonStyle = {{ marginVertical: '5%' }}
-				onPress = { () => { navigation.navigate('Login') } }
-			/>
+			<View style = { buttons }>
+				<Button
+					title = 'Collection'
+					containerStyle = { buttonContainer }
+					buttonStyle = {{ width: '100%', height: '100%' }}
+				/>
+				<Button
+					title = 'My Account'
+					containerStyle = { buttonContainer }
+					buttonStyle = {{ width: '100%', height: '100%' }}
+				/>
+			</View>
 		</View>
 	);
 };
@@ -24,7 +29,8 @@ const styles = {
 		height: '100%',
 		width: '100%',
 		justifyContent: 'center',
-		alignItems: 'center'
+		alignItems: 'center',
+		position: 'relative'
 	},
 	textStyle: {
 		backgroundColor: 'white',
@@ -32,7 +38,20 @@ const styles = {
 		width: '100%',
 		justifyContent: 'center',
 		alignItems: 'center'
+	},
+	buttons: {
+		width: '90%',
+		height: '10%',
+		alignItems: 'center',
+		flexDirection: 'row',
+		justifyContent: 'space-between',
+		top: '83%'
+	},
+	buttonContainer: {
+		width: '40%',
+		height: '60%',
+		justifyContent: 'center'
 	}
-}
+};
 
 export default Main;
