fe28277a4255e993af6ff755d0143f83e65e24ff
Jordan Schneider <60559211+schneidercorn@users.noreply.github.com>
2021-02-24 00:00:00 -0500

    actually added the library for realsies

--- android/app/src/main/java/com/spotapp/MainApplication.java
+++ android/app/src/main/java/com/spotapp/MainApplication.java
@@ -8,6 +8,7 @@ import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
+import org.reactnative.camera.RNCameraPackage;
 import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
@@ -27,6 +28,9 @@ public class MainApplication extends Application implements ReactApplication {
           List<ReactPackage> packages = new PackageList(this).getPackages();
           // Packages that cannot be autolinked yet can be added manually here, for example:
           // packages.add(new MyReactNativePackage());
+
+          packages.add(new RNCameraPackage());
+          
           return packages;
         }
 
